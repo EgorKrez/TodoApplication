a76b516ad409b18fb6064e0593d9444043168b6c
Yahor Krez <ykrez@gomel.iba.by>
2020-11-10 21:25:35 +0300

    Todo app has been added

---
+++ ng-app/src/app/shared/todos-filter.pipe.ts
@@ -0,0 +1,19 @@
+import {Pipe, PipeTransform} from '@angular/core'
+import { from } from 'rxjs'
+import {Todo} from './todos.service'
+
+@Pipe({
+    name: 'todosFilter'
+})
+
+export class TodosFilterPipe implements PipeTransform{
+    transform(todos: Todo[], search: string = ''): Todo[] {
+    if (!search.trim()) {
+        return todos
+    }
+
+    return todos.filter(todo => {
+        return todo.title.toLocaleLowerCase().indexOf(search.toLocaleLowerCase()) !== -1
+    })
+    }
+}
